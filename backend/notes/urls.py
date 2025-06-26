654b5a2fc11a2e762f6dfb91e183e27d25731dab
Egba Frank <Egbafrank72@gmail.com>
2025-06-26 23:07:57 +0100

    fix(notes): Remove unnecessary comments and clean up initial migration and model files

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import NoteListCreateAPIView # Import your new class-based view
 
 urlpatterns = [
-feature-backend/implement-notes-api
     # This single path now handles both GET (list notes) and POST (create note)
 
     path('', NoteListCreateAPIView.as_view(), name='note-list-create'),
