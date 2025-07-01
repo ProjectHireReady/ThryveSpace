607da137f7a5441f15d55c0cfdb65453734bc082
NomsuBen <olaniranbenjamin@gmail.com>
2025-07-01 10:13:38 +0100

    feat: Implement notes detail views and serializers for edit/delete

---
+++ backend/users/serializers.py
@@ -0,0 +1,8 @@
+# backend/users/serializers.py
+from rest_framework import serializers
+from .models import CustomUser # Make sure to import your CustomUser model
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email'] # Or whatever fields you want to expose
