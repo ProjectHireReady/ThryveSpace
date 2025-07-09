372ea2290a23bb7902ceaa8c489bef30397b6540
Egba Frank <Egbafrank72@gmail.com>
2025-07-09 23:41:04 +0100

    refactor(users): rename reset-token endpoint to recover and update docs

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -9,7 +9,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("", GuestCreateView.as_view(), name="create_guest"),
-    path("reset-token/", ResetTokenView.as_view(), name="reset_token"),
+    path("recover/", ResetTokenView.as_view(), name="recover"),
     path("upgrade/", GuestUpgradeView.as_view(), name="upgrade_guest"),
     path("login/", obtain_auth_token, name="user_login"),
     path("logout/", LogoutView.as_view(), name="user_logout"),
