0b95d76dbc67aa2c133d23f62e4ca6822b2134d5
Egba Frank <Egbafrank72@gmail.com>
2025-07-29 22:11:59 +0100

    refactor(user): remove redundant imports

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,6 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from .models import CustomUser
