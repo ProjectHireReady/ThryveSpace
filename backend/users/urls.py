4c3f421cce0383bdc277aafc8fce09c5c287e994
EgbaFrank <125460843+EgbaFrank@users.noreply.github.com>
2025-08-02 16:32:51 +0100

    ♻️ Refactor: Remove Guest Auth System and Implement Clean User Signup Flow (#37)
    * refactor(user): remove guest-related fields
    
    * fix(urls): change path for user signup
    
    * refactor (user): remove unused guest-related serializers and views
    
    * refactor(user): remove guest-related fields
    
    * refactor(user): remove redundant imports

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,18 +1,12 @@
 from django.urls import path
 from .views import (
-    GuestCreateView,
-    ResetTokenView,
-    GuestUpgradeView,
     LogoutView,
     SignUpView,
+    LoginView,
 )
-from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path("", GuestCreateView.as_view(), name="create_guest"),
-    path("recover/", ResetTokenView.as_view(), name="recover"),
-    path("upgrade/", GuestUpgradeView.as_view(), name="upgrade_guest"),
-    path("login/", obtain_auth_token, name="user_login"),
+    path("login/", LoginView.as_view(), name="user_login"),
     path("logout/", LogoutView.as_view(), name="user_logout"),
-    path("register/", SignUpView.as_view(), name="user_signup"),
+    path("signup/", SignUpView.as_view(), name="user_signup"),
 ]
