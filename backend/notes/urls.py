e9735648bdf6c568cd53666bf2a6482ef8b56fb5
buyekeobare <buyekeobare@gmail.com>
2025-08-02 23:41:14 +0300

    Simplify notes URLs by removing redundant prefix

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import NoteListCreateAPIView, NoteDetailAPIView  # Import the new view
 
 urlpatterns = [
-    path("notes/", NoteListCreateAPIView.as_view(), name="note-list-create"),
+    path("", NoteListCreateAPIView.as_view(), name="note-list-create"),
     path(
-        "notes/<uuid:id>/", NoteDetailAPIView.as_view(), name="note-detail"
+        "<uuid:id>/", NoteDetailAPIView.as_view(), name="note-detail"
     ),  # New URL pattern
 ]
