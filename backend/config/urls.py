a78ae5f8bf7c4777d6021b2e09894c6b5d6f4599
buyekeobare <buyekeobare@gmail.com>
2025-08-02 22:39:48 +0300

    Add DRF token authentication endpoint at /api/v1/token/

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/moods/', include('moods.urls')),
     path('api/v1/notes/', include('notes.urls')),
     path('api/v1/auth/', include('users.urls')),
+    path('api/v1/token/', obtain_auth_token, name='api_token_auth'),
+
 ]
