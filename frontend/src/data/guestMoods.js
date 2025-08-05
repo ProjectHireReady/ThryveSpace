a9e9a2bb97efbb169040b171e63f4f29fdfe3217
Nobie12 <kenaule1@gmail.com>
2025-08-05 15:29:26 +0300

    fetching moods

---
+++ frontend/src/data/guestMoods.js
@@ -0,0 +1,17 @@
+// src/data/moods.js
+export const guestMoods = [
+  {
+    id: "happy-1",
+    name: "Happy",
+    emoji: "😊",
+    image_url:
+      "https://res.cloudinary.com/your-cloud/image/upload/v123/happy.png", //sample data for now
+  },
+  {
+    id: "sad-1",
+    name: "Sad",
+    emoji: "😢",
+    image_url:
+      "https://res.cloudinary.com/your-cloud/image/upload/v123/sad.png", //sample data for now
+  },
+];
