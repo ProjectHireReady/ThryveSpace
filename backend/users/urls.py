e5feb90de9495eb6946ecbfae38e7760fe59b8b9
Egba Frank <Egbafrank72@gmail.com>
2025-08-25 15:29:12 +0100

    feat(users): Add endpoint for retrieving and updating user details

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,9 +3,11 @@ from .views import (
     LogoutView,
     SignUpView,
     LoginView,
+    UserMeView
 )
 
 urlpatterns = [
+    path("me/", UserMeView.as_view(), name="user_me"),
     path("login/", LoginView.as_view(), name="user_login"),
     path("logout/", LogoutView.as_view(), name="user_logout"),
     path("signup/", SignUpView.as_view(), name="user_signup"),
