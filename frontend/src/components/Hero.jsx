c0dd1a60c9f83f8ff61d52592ca35e34bd73fbbc
STARR44 <okekeesther100@gmail.com>
2025-08-26 13:23:33 +0100

    refactor(ui): adjust spacing, colors, and icons in major components

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 // Hero.jsx
 import { useEffect } from "react";
-import { Link } from "react-router-dom"; //navigate to mood screen
+import { Link } from "react-router-dom";
 import heroBg from "../assets/hero-img.svg";
 import "./Hero.css";
 
@@ -12,15 +12,13 @@ function Hero() {
 
   return (
     <section className="hero section">
-      <img src={heroBg} alt="Hero background" className="hero-bg-img" />
       <div className="overlay"></div>
       <div className="hero-content">
         <div className="text-group">
           <h1>Mental Wellness Buddy</h1>
           <p>
-            Track moods. reflect calmly. <br />A private gentle space for you.
+            Track moods. reflect calmly. <br /> A private gentle space for you.
           </p>
-
           <Link to="/mood">
             <button className="hero-btn button">Track Mood</button>
           </Link>
