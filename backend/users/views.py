2e13944b52bcb78e66fe139c90a07a28a0bf9d44
EgbaFrank <125460843+EgbaFrank@users.noreply.github.com>
2025-09-04 20:10:08 +0100

    Fix login to accept email authentication

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,7 +6,11 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from .models import CustomUser
-from .serializers import SignUpSerializer, CustomUserSerializer
+from .serializers import (
+    SignUpSerializer,
+    CustomUserSerializer,
+    EmailAuthTokenSerializer,
+)
 
 
 class UserMeView(generics.RetrieveUpdateAPIView):
@@ -23,6 +27,7 @@ class LoginView(ObtainAuthToken):
     """
 
     permission_classes = [AllowAny]
+    serializer_class = EmailAuthTokenSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(
