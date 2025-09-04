c55395a6479ad0325dad4df66eda57590483f47b
Egba Frank <Egbafrank72@gmail.com>
2025-09-04 15:34:53 +0100

    fix(auth): Fix login to accept email authentication

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework import serializers
 from .models import CustomUser
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 User = get_user_model()
+
+
+class EmailAuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+
+        if email and password:
+            # Note: The 'authenticate' function is called with 'email' instead of 'username'.
+            user = authenticate(
+                request=self.context.get("request"), email=email, password=password
+            )
+
+            if not user:
+                raise serializers.ValidationError("Invalid credentials.")
+        else:
+            raise serializers.ValidationError("Both email and password are required.")
+
+        data["user"] = user
+        return data
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
