175748b3d7914fb6e563493a9ee6e32c5b32db64
Egba Frank <Egbafrank72@gmail.com>
2025-10-06 21:47:28 +0100

    fix(settings): update STATIC_URL to include leading slash

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,7 +137,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
