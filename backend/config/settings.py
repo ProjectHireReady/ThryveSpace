15ab571d79b743ddfdc67640d9456684a142a988
Egba Frank <Egbafrank72@gmail.com>
2025-10-06 21:23:53 +0100

    fix(settings): update ALLOWED_HOSTS to use environment variable

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 OPENAI_API_KEY = config("OPENAI_API_KEY", default="")
-ALLOWED_HOSTS = []  # set in env for non-local
+ALLOWED_HOSTS = [
+    host for host in config("ALLOWED_HOSTS", default="").split(",") if host
+]
 
 # Application definition
 INSTALLED_APPS = [
