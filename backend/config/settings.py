380043729f7fa299fff5d3f3470012075adb3983
Egba Frank <Egbafrank72@gmail.com>
2025-10-06 22:06:24 +0100

    feat(settings): add WhiteNoise middleware and configure static file settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,6 +54,7 @@ AUTH_USER_MODEL = "users.CustomUser"
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -138,6 +139,8 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
